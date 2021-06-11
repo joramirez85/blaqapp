d0a572fcd85064373f7a2594830ead21ce6b46e9
Omar Ramirez <jramirez@foundationmedicine.com>
2021-06-11 03:50:43 -0500

    personal info

--- api/controllers/personalInfo.js
+++ api/controllers/personalInfo.js
@@ -28,3 +28,55 @@ exports.personal_info = (req, res, next) => {
       })
   })
 }
+
+exports.get_personal_info = (req, res, next) => {
+  const email = req.params.email
+  PersonalInfo.find({email})
+  .exec()
+  .then(doc => {
+    console.log('doc saved: ', doc)
+    if (doc){
+        res.status(200).json({
+            info: doc,
+            request: {
+                type: 'GET',
+                description: 'personal info',
+            }
+        })
+    } else {
+        res.status(404).json({
+            message: 'No valid entry found for provided Email'
+        })
+    } 
+  }).catch(err => {
+    console.log('error: ', err)
+    res.status(500).json({
+        error: err
+    })
+  })
+}
+
+exports.update_personal_info = (req, res, next) => {
+  const email = req.params.email
+  const updateOps = {}
+  for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value
+  }
+
+  PersonalInfo.update({email}, { $set: updateOps})
+  .exec()
+  .then(result => {
+    res.status(200).json({
+        message: 'info updated',
+        request: {
+            type: 'POST',
+        }
+    })
+  })
+  .catch(err => {
+    console.log('error: ', err)
+    res.status(500).json({
+        error: err
+    })
+  })
+}
--- api/routes/personalInfo.js
+++ api/routes/personalInfo.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const PersonalInfoController = require('../controllers/personalInfo')
 
 router.post('/personal_info', PersonalInfoController.personal_info)
+router.get('/personal_info/:email', PersonalInfoController.get_personal_info)
+router.put('/personal_info/:email', PersonalInfoController.update_personal_info)
 
 module.exports = router
